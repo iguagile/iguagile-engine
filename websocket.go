06e435ede9d6344ee0bb981229d7643e6a5eb774
bootjp <oh@bootjp.me>
2019-02-07 15:23:12 +0900

    change websocket framework

--- websocket.go
+++ websocket.go
@@ -3,36 +3,43 @@ package main
 import (
 	"net/http"
 
-	"github.com/labstack/gommon/log"
+	"golang.org/x/net/websocket"
 
 	"github.com/labstack/echo"
-	"gopkg.in/olahol/melody.v1"
 )
 
 func main() {
-	logger := log.Logger{}
-	m := melody.New()
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		http.ServeFile(c.Response().Writer, c.Request(), "index.html")
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.GET("/ws", func(c echo.Context) error {
-		if err := m.HandleRequest(c.Response().Writer, c.Request()); err != nil {
-			logger.Fatal()
-		}
 		return nil
 	})
 
-	m.HandleMessage(func(s *melody.Session, msg []byte) {
-		if err := m.Broadcast(msg); err != nil {
-			logger.Fatal(err)
-		}
+	e.GET("/ws", func(c echo.Context) error {
+		websocket.Handler(func(ws *websocket.Conn) {
+			defer func() {
+				if err := ws.Close(); err != nil {
+					c.Logger().Error(err)
+				}
+			}()
+			for {
+				// Read
+				msg := ""
+				err := websocket.Message.Receive(ws, &msg)
+				if err != nil {
+					c.Logger().Warn(err)
+				}
 
+				err = websocket.Message.Send(ws, msg)
+				if err != nil {
+					c.Logger().Warn(err)
+				}
+			}
+		}).ServeHTTP(c.Response(), c.Request())
+		return nil
 	})
+
 	if err := e.Start(":5000"); err != nil {
-
-		logger.Fatal(err)
-
+		e.Logger.Fatal(err)
 	}
 }
