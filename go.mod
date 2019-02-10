aed28aece16e6ce0daea88fc10157e8a73ea7195
bootjp <oh@bootjp.me>
2019-02-10 22:15:57 +0900

    apply gorilla/websocket style implements
    ToDo error handling.

--- go.mod
+++ go.mod
@@ -1,7 +1,7 @@
 module github.com/iguagile/iguagile-engine
 
 require (
-	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/gorilla/websocket v1.4.0
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.2.8 // indirect
 	github.com/mattn/go-colorable v0.1.0 // indirect
@@ -9,7 +9,6 @@ require (
 	github.com/stretchr/testify v1.3.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v0.0.0-20170224212429-dcecefd839c4 // indirect
-	golang.org/x/crypto v0.0.0-20190131182504-b8fe1690c613 // indirect
+	golang.org/x/crypto v0.0.0-20190208162236-193df9c0f06f // indirect
 	golang.org/x/sys v0.0.0-20190116161447-11f53e031339 // indirect
-	gopkg.in/olahol/melody.v1 v1.0.0-20170518105555-d52139073376
 )
--- websocket.go
+++ websocket.go
@@ -3,12 +3,15 @@ package main
 import (
 	"net/http"
 
-	"golang.org/x/net/websocket"
+	"github.com/iguagile/iguagile-engine/hub"
 
 	"github.com/labstack/echo"
 )
 
 func main() {
+
+	h := hub.NewHub()
+	go h.Run()
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		http.ServeFile(c.Response().Writer, c.Request(), "index.html")
@@ -16,30 +19,12 @@ func main() {
 	})
 
 	e.GET("/ws", func(c echo.Context) error {
-		websocket.Handler(func(ws *websocket.Conn) {
-			defer func() {
-				if err := ws.Close(); err != nil {
-					c.Logger().Error(err)
-				}
-			}()
-			for {
-				// Read
-				msg := ""
-				err := websocket.Message.Receive(ws, &msg)
-				if err != nil {
-					c.Logger().Warn(err)
-				}
-
-				err = websocket.Message.Send(ws, msg)
-				if err != nil {
-					c.Logger().Warn(err)
-				}
-			}
-		}).ServeHTTP(c.Response(), c.Request())
+		hub.ServeWs(h, c.Response().Writer, c.Request())
 		return nil
 	})
 
 	if err := e.Start(":5000"); err != nil {
 		e.Logger.Fatal(err)
 	}
+
 }
