580bea8003d2084d99bb25cde493ec0aa9e36399
bootjp <oh@bootjp.me>
2019-03-23 18:16:10 +0900

    WIP In rare cases, the message becomes null byte

--- websocket_test.go
+++ websocket_test.go
@@ -8,94 +8,106 @@ import (
 	"testing"
 	"time"
 
-	"github.com/iguagile/iguagile-engine/hub"
-
 	"github.com/gorilla/websocket"
+	"github.com/iguagile/iguagile-engine/hub"
 )
 
 func TestConnection(t *testing.T) {
 
-	srv := &http.Server{
-		Addr: "127.0.0.1:5000",
+	for i := 0; i < 10; i++ {
+
+		srv := &http.Server{
+			Addr: "127.0.0.1:5000",
+		}
+
+		go func(t *testing.T) {
+
+			h := hub.NewHub()
+			go h.Run()
+			f := func(w http.ResponseWriter, r *http.Request) {
+				hub.ServeWs(h, w, r)
+			}
+			srv.Handler = http.HandlerFunc(f)
+
+			if err := srv.ListenAndServe(); err != nil {
+				t.Log(err)
+			}
+		}(t)
+
+		time.Sleep(200 * time.Millisecond)
+		fmt.Println("RUN")
+		wg := &sync.WaitGroup{}
+		wg.Add(2)
+
+		go func(t *testing.T, wg *sync.WaitGroup) {
+			u := url.URL{Scheme: "ws", Host: "127.0.0.1:5000", Path: "/"}
+			ws, resp, err := websocket.DefaultDialer.Dial(u.String(), nil)
+
+			if err != nil {
+				t.Logf("handshake failed with status %d", resp.StatusCode)
+				t.Fatalf("%v", err)
+			}
+
+			messageType, p, err := ws.ReadMessage()
+			if err != nil {
+				t.Fatalf("%v", err)
+			}
+
+			if messageType != websocket.BinaryMessage {
+				t.Error("support binary message only")
+				t.FailNow()
+			}
+			t.Log(len(p))
+			t.Logf("%v\n", p)
+			t.Logf("%s\n", p)
+			// remove uuid
+			received := p[36:]
+			// remove mesType
+			data := received[1:]
+
+			if "hello1" != string(data) {
+				t.Error("bad message")
+				t.Errorf("%v\n", data)
+				t.Errorf("%s\n", data)
+				//t.FailNow()
+			}
+			t.Log(string(data))
+
+			wg.Done()
+			wg.Wait()
+			if err := ws.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, "")); err != nil {
+				t.Errorf("%v", err)
+			}
+			time.Sleep(50 * time.Microsecond)
+			_ = ws.Close()
+
+		}(t, wg)
+
+		go func(t *testing.T, wg *sync.WaitGroup) {
+			u := url.URL{Scheme: "ws", Host: "127.0.0.1:5000", Path: "/"}
+			ws, resp, err := websocket.DefaultDialer.Dial(u.String(), nil)
+			if err != nil {
+				t.Logf("handshake failed with status %d", resp.StatusCode)
+				t.Errorf("%v", err)
+			}
+
+			data := []byte("00hello1")
+			if err := ws.WriteMessage(websocket.BinaryMessage, data); err != nil {
+				t.Errorf("%v", err)
+			}
+			t.Logf("send %v %s\n", data, data)
+
+			wg.Done()
+			wg.Wait()
+			if err := ws.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, "")); err != nil {
+				t.Errorf("%v", err)
+			}
+
+			time.Sleep(50 * time.Millisecond)
+			_ = ws.Close()
+		}(t, wg)
+		wg.Wait()
+		time.Sleep(200 * time.Millisecond)
+		srv.Close()
 	}
-
-	go func(t *testing.T) {
-
-		h := hub.NewHub()
-		go h.Run()
-		f := func(w http.ResponseWriter, r *http.Request) {
-			hub.ServeWs(h, w, r)
-		}
-		srv.Handler = http.HandlerFunc(f)
-
-		if err := srv.ListenAndServe(); err != nil {
-			t.Error(err)
-		}
-	}(t)
-
-	time.Sleep(1 * time.Second)
-	fmt.Println("RUN")
-	wg := &sync.WaitGroup{}
-	wg.Add(2)
-
-	go func(t *testing.T, wg *sync.WaitGroup) {
-		u := url.URL{Scheme: "ws", Host: "127.0.0.1:5000", Path: "/ws"}
-		ws, resp, err := websocket.DefaultDialer.Dial(u.String(), nil)
-		//ws, resp, err := dialer.Dial(u, nil)
-		if err == websocket.ErrBadHandshake {
-			t.Logf("handshake failed with status %d", resp.StatusCode)
-			t.Fatalf("%v", err)
-		}
-
-		defer func(ws *websocket.Conn, wg *sync.WaitGroup) {
-			if err := ws.Close(); err != nil {
-				t.Error(err)
-			}
-		}(ws, wg)
-
-		messageType, p, err := ws.ReadMessage()
-		if err != nil {
-			t.Fatalf("%v", err)
-		}
-
-		if messageType != websocket.BinaryMessage {
-			t.Fail()
-		}
-		// remove uuid
-		received := p[16:]
-		// remove mesType
-		data := received[1:]
-
-		if "hello1" != string(data) {
-			t.Error("bad message")
-			t.Errorf("%v", data)
-			t.Errorf("%s", data)
-		}
-		t.Log(string(data))
-		wg.Done()
-
-	}(t, wg)
-
-	go func(t *testing.T, wg *sync.WaitGroup) {
-		u := url.URL{Scheme: "ws", Host: "127.0.0.1:5000", Path: "/"}
-		ws, resp, err := websocket.DefaultDialer.Dial(u.String(), nil)
-		if err == websocket.ErrBadHandshake {
-			t.Logf("handshake failed with status %d", resp.StatusCode)
-			t.Errorf("%v", err)
-		}
-		defer func(ws *websocket.Conn, wg *sync.WaitGroup) {
-			if err := ws.Close(); err != nil {
-				t.Error(err)
-			}
-		}(ws, wg)
-
-		data := []byte("11hello1")
-		if err := ws.WriteMessage(websocket.BinaryMessage, data); err != nil {
-			t.Errorf("%v", err)
-		}
-		wg.Done()
-
-	}(t, wg)
-	wg.Wait()
-	srv.Close()
 }
