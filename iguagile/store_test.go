b47409b9c5ef7cc0d6a4af9a31b239c7cf81294e
bootjp <oh@bootjp.me>
2019-06-23 19:49:22 +0900

    fix test condition, and close connection

--- iguagile/store_test.go
+++ iguagile/store_test.go
@@ -8,11 +8,15 @@ var sid int
 
 func TestCanGenerateServerID(t *testing.T) {
 	var store = NewRedis("localhost:6379")
+	defer func() {
+		_ = store.Close()
+	}()
 	id, err := store.GenerateServerID()
 	if err != nil {
 		t.Error(err)
 	}
-	if id <= 1>>16 {
+
+	if (id & 0xffff) != 0 {
 		t.Errorf("invalid server id %b", id)
 	}
 	sid = id
@@ -20,11 +24,16 @@ func TestCanGenerateServerID(t *testing.T) {
 
 func TestCanGenerateRoomID(t *testing.T) {
 	var store = NewRedis("localhost:6379")
+	defer func() {
+		_ = store.Close()
+	}()
+
 	id, err := store.GenerateRoomID(sid)
 	if err != nil {
 		t.Error(err)
 	}
-	if id <= sid {
+
+	if (id & 0xffff0000) != sid {
 		t.Errorf("invalid server id %b", id)
 	}
 }
