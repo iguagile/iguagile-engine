76dcfa8f9fda9cbca64e7aae8de1a17a2fa135a1
minami14 <minami-git@minami-vr.sakura.ne.jp>
2019-07-12 19:51:55 +0900

    refactor *ClientWebsocket.Run

--- iguagile/websocket-client.go
+++ iguagile/websocket-client.go
@@ -2,8 +2,6 @@ package iguagile
 
 import (
 	"encoding/binary"
-	"time"
-
 	"github.com/gorilla/websocket"
 )
 
@@ -35,8 +33,29 @@ func NewClientWebsocket(room *Room, conn *websocket.Conn) (*ClientWebsocket, err
 
 // Run is provides backend synchronize goroutine.
 func (c *ClientWebsocket) Run() {
-	go c.readPump()
-	go c.writePump()
+	go func() {
+		for {
+			_, message, err := c.conn.ReadMessage()
+			if err != nil {
+				c.room.log.Println(err)
+				c.room.CloseConnection(c)
+				break
+			}
+
+			c.room.Receive(c, message)
+		}
+	}()
+
+	go func() {
+		for {
+			message := <-c.send
+			if err := c.conn.WriteMessage(websocket.BinaryMessage, message); err != nil {
+				c.room.log.Println(err)
+				c.room.CloseConnection(c)
+				break
+			}
+		}
+	}()
 }
 
 // GetID is getter for id
@@ -56,84 +75,4 @@ func (c *ClientWebsocket) Send(message []byte) {
 
 func (c *ClientWebsocket) Close() error {
 	return c.conn.Close()
-}
-
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// https://github.com/gorilla/websocket/blob/master/LICENSE
-
-func (c *ClientWebsocket) readPump() {
-	defer func() {
-		c.room.CloseConnection(c)
-	}()
-
-	c.conn.SetReadLimit(maxMessageSize)
-	if err := c.conn.SetReadDeadline(time.Now().Add(pongWait)); err != nil {
-		c.room.log.Println(err)
-	}
-
-	c.conn.SetPongHandler(func(string) error {
-		if err := c.conn.SetReadDeadline(time.Now().Add(pongWait)); err != nil {
-			c.room.log.Printf("error: %v", err)
-		}
-		return nil
-	})
-	for {
-		_, message, err := c.conn.ReadMessage()
-		if err != nil {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				c.room.log.Printf("error: %v", err)
-			}
-			break
-		}
-
-		c.room.Receive(c, message)
-	}
-}
-
-func (c *ClientWebsocket) writePump() {
-	ticker := time.NewTicker(pingPeriod)
-	defer func() {
-		ticker.Stop()
-		c.room.CloseConnection(c)
-	}()
-	for {
-		select {
-		case message, ok := <-c.send:
-			if err := c.conn.SetWriteDeadline(time.Now().Add(writeWait)); err != nil {
-				c.room.log.Println(err)
-			}
-			if !ok {
-				// The room closed the channel.
-				if err := c.conn.WriteMessage(websocket.CloseMessage, []byte{}); err != nil {
-					c.room.log.Println(err)
-				}
-
-				return
-			}
-
-			w, err := c.conn.NextWriter(websocket.BinaryMessage)
-			if err != nil {
-				c.room.log.Println(err)
-				return
-			}
-
-			if _, err := w.Write(message); err != nil {
-				c.room.log.Println(err)
-			}
-
-			if err := w.Close(); err != nil {
-				c.room.log.Println(err)
-				return
-			}
-		case <-ticker.C:
-			if err := c.conn.SetWriteDeadline(time.Now().Add(writeWait)); err != nil {
-				c.room.log.Println(err)
-			}
-
-			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
-				return
-			}
-		}
-	}
 }
