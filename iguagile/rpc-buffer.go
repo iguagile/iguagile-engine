a1b717535e04ff780e11a839fef39c94a5a5387b
minami14 <minami-git@minami-vr.sakura.ne.jp>
2019-08-14 20:50:01 +0900

    add comment

--- iguagile/rpc-buffer.go
+++ iguagile/rpc-buffer.go
@@ -4,11 +4,13 @@ import (
 	"sync"
 )
 
+// RPCBufferManager manages buffered rpc messages.
 type RPCBufferManager struct {
 	buffer map[*[]byte]*Client
 	sync.Mutex
 }
 
+// NewRPCBufferManager is RPCBufferManger constructed.
 func NewRPCBufferManager() *RPCBufferManager {
 	return &RPCBufferManager{
 		buffer: make(map[*[]byte]*Client),
@@ -16,12 +18,14 @@ func NewRPCBufferManager() *RPCBufferManager {
 	}
 }
 
+// Add new rpc message.
 func (m *RPCBufferManager) Add(message []byte, sender *Client) {
 	m.Lock()
 	m.buffer[&message] = sender
 	m.Unlock()
 }
 
+// Remove rpc messages.
 func (m *RPCBufferManager) Remove(client *Client) {
 	m.Lock()
 	for buffer, sender := range m.buffer {
@@ -32,12 +36,14 @@ func (m *RPCBufferManager) Remove(client *Client) {
 	m.Unlock()
 }
 
+// Clear all rpc messages.
 func (m *RPCBufferManager) Clear() {
 	m.Lock()
 	m.buffer = make(map[*[]byte]*Client)
 	m.Unlock()
 }
 
+// SendRPCBuffer sends all buffered rpc messages.
 func (m *RPCBufferManager) SendRPCBuffer(client *Client) {
 	m.Lock()
 	for buffer := range m.buffer {
