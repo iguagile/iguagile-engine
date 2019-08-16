5ec48471e4f5bac978d3e3d9ec7881cfff9e4578
minami14 <minami-git@minami-vr.sakura.ne.jp>
2019-08-16 14:39:20 +0900

    add comment

--- iguagile/id.go
+++ iguagile/id.go
@@ -6,10 +6,12 @@ import (
 	"github.com/minami14/idgo"
 )
 
+// IDGenerator generates id.
 type IDGenerator struct {
 	generator *idgo.IDGenerator
 }
 
+// NewIDGenerator is IDGenerator constructed.
 func NewIDGenerator() (*IDGenerator, error) {
 	store, err := idgo.NewLocalStore(math.MaxInt16)
 	if err != nil {
@@ -24,10 +26,12 @@ func NewIDGenerator() (*IDGenerator, error) {
 	return &IDGenerator{generator}, nil
 }
 
+// Generate generates a id.
 func (g *IDGenerator) Generate() (int, error) {
 	return g.generator.Generate()
 }
 
+// Free deallocates the id.
 func (g *IDGenerator) Free(id int) error {
 	return g.generator.Free(id)
 }
