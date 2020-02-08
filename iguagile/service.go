7cf3eedaa6b1ae86ec95770bd2cb8722fb8cc85d
minami14 <minami-git@minami-vr.sakura.ne.jp>
2020-02-08 15:20:39 +0900

    create RelayService

--- iguagile/service.go
+++ iguagile/service.go
@@ -24,6 +24,45 @@ type RoomServiceFactory interface {
 	Create(room *Room) (RoomService, error)
 }
 
+// RelayService is a service relays data.
+type RelayService struct {
+	room *Room
+}
+
+// Receive receives data and sends to all clients.
+func (s *RelayService) Receive(senderID int, data []byte) error {
+	s.room.SendToAllClients(senderID, data)
+	return nil
+}
+
+// OnRegisterClient for implement RoomService.
+func (s *RelayService) OnRegisterClient(_ int) error {
+	return nil
+}
+
+// OnUnregisterClient for implement RoomService.
+func (s *RelayService) OnUnregisterClient(_ int) error {
+	return nil
+}
+
+// OnChangeHost for implement RoomService.
+func (s RelayService) OnChangeHost(_ int) error {
+	return nil
+}
+
+// Destroy for implement RoomService.
+func (s RelayService) Destroy() error {
+	return nil
+}
+
+// RelayServiceFactory creates RelayService.
+type RelayServiceFactory struct{}
+
+// Create creates a EmptyRoomService.
+func (f RelayServiceFactory) Create(room *Room) (RoomService, error) {
+	return &RelayService{room: room}, nil
+}
+
 // EmptyRoomService is empty service used for relay server.
 type EmptyRoomService struct{}
 
