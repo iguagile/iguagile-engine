abe8f6181e2006d00696b6e7e2702ae2359a37c1
Minami <minami-git@minami-vr.sakura.ne.jp>
2021-02-21 16:36:02 +0900

    add interfaces for quic

---
+++ iguagile/conn.go
@@ -0,0 +1,71 @@
+package iguagile
+
+import (
+	"context"
+	"errors"
+	"io"
+	"net"
+
+	"github.com/lucas-clemente/quic-go"
+)
+
+type Stream io.ReadWriteCloser
+
+type Conn interface {
+	AcceptStream() (Stream, error)
+	OpenStream() (Stream, error)
+	ReceiveMessage() ([]byte, error)
+	SendMessage([]byte) error
+	Close() error
+}
+
+type Listener interface {
+	Accept() (Conn, error)
+	Addr() net.Addr
+	Close() error
+}
+
+type quicConn struct {
+	sess quic.Session
+}
+
+func (q *quicConn) AcceptStream() (Stream, error) {
+	return q.sess.AcceptStream(context.Background())
+}
+
+func (q *quicConn) OpenStream() (Stream, error) {
+	return q.sess.OpenStream()
+}
+
+func (q *quicConn) ReceiveMessage() ([]byte, error) {
+	return nil, errors.New("not implemented")
+}
+
+func (q *quicConn) SendMessage(buf []byte) error {
+	return errors.New("not implemented")
+}
+
+func (q *quicConn) Close() error {
+	return nil
+}
+
+type quicListener struct {
+	listener quic.Listener
+}
+
+func (q *quicListener) Accept() (Conn, error) {
+	sess, err := q.listener.Accept(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	return &quicConn{sess}, nil
+}
+
+func (q *quicListener) Addr() net.Addr {
+	return q.listener.Addr()
+}
+
+func (q *quicListener) Close() error {
+	return q.listener.Close()
+}
