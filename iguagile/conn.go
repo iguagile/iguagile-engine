0f80a303584f40b558ec4143692a1f8ea4f6921b
minami14 <minami-git@minami-vr.sakura.ne.jp>
2021-02-23 17:33:03 +0900

    add quicStream

--- iguagile/conn.go
+++ iguagile/conn.go
@@ -2,6 +2,8 @@ package iguagile
 
 import (
 	"context"
+	"crypto/tls"
+	"encoding/binary"
 	"errors"
 	"io"
 	"net"
@@ -28,16 +30,65 @@ type Listener interface {
 	Close() error
 }
 
+type quicStream struct {
+	stream quic.Stream
+}
+
+func (q *quicStream) Read(buf []byte) (int, error) {
+	_, err := q.stream.Read(buf[:2])
+	if err != nil {
+		return 0, err
+	}
+
+	size := int(binary.LittleEndian.Uint16(buf))
+	receivedSizeSum := 0
+	for receivedSizeSum < size {
+		receivedSize, err := q.stream.Read(buf[receivedSizeSum:size])
+		if err != nil {
+			return 0, err
+		}
+
+		receivedSizeSum += receivedSize
+	}
+
+	return size, nil
+}
+
+func (q *quicStream) Write(buf []byte) (int, error) {
+	size := len(buf)
+	sizeByte := make([]byte, 2, size+2)
+	binary.LittleEndian.PutUint16(sizeByte, uint16(size))
+	buf = append(sizeByte, buf...)
+	if _, err := q.stream.Write(buf); err != nil {
+		return 0, err
+	}
+	return size, nil
+}
+
+func (q *quicStream) Close() error {
+	return q.stream.Close()
+}
+
 type quicConn struct {
 	sess quic.Session
 }
 
 func (q *quicConn) AcceptStream() (Stream, error) {
-	return q.sess.AcceptStream(context.Background())
+	stream, err := q.sess.AcceptStream(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	return &quicStream{stream}, nil
 }
 
 func (q *quicConn) OpenStream() (Stream, error) {
-	return q.sess.OpenStream()
+	stream, err := q.sess.OpenStream()
+	if err != nil {
+		return nil, err
+	}
+
+	return &quicStream{stream}, nil
 }
 
 func (q *quicConn) ReceiveMessage() ([]byte, error) {
@@ -54,6 +105,15 @@ func (q *quicConn) Close() error {
 
 type quicListener struct {
 	listener quic.Listener
+}
+
+func ListenQuic(address string, tlsConfig *tls.Config) (Listener, error) {
+	l, err := quic.ListenAddr(address, tlsConfig, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &quicListener{l}, nil
 }
 
 func (q *quicListener) Accept() (Conn, error) {
