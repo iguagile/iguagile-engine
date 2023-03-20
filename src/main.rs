9d4850252268cafd1340079e3a1b313042ac1d99
bootjp / Yoshiaki Ueda <contact@bootjp.me>
2023-03-20 23:19:26 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use crate::store::MemoryStore;
 mod store;
 
 fn main() {
-    let m = MemoryStore::new();
+    let redis = redis::Client::open("redis://127.0.0.1").unwrap();
+    let m = MemoryStore::new(redis);
     let _ = m;
     println!("Hello, world!");
 }
--- src/store.rs
+++ src/store.rs
@@ -1,7 +1,6 @@
 use std::io;
-use std::sync::atomic;
 
-use iguagile::{Server, Room};
+use iguagile::{Room, Server};
 
 pub mod iguagile {
     tonic::include_proto!("iguagile");
@@ -9,30 +8,41 @@ pub mod iguagile {
 
 trait Store {
     fn generate_server_id(&self) -> Result<i64, io::Error>;
-    fn register_server(&self, s : Server) -> Result<(), io::Error>;
+    fn register_server(&self, s: Server) -> Result<(), io::Error>;
     fn unregister_server(&self, s: Server) -> Result<(), io::Error>;
     fn register_room(&self, r: Room) -> Result<(), io::Error>;
     fn unregister_room(&self, r: Room) -> Result<(), io::Error>;
 }
 
-#[derive(Default)]
 pub struct MemoryStore {
-    server_id: atomic::AtomicI64,
+    redis: redis::Client,
 }
 
 impl MemoryStore {
-    pub fn new() -> Self {
-        MemoryStore {
-            server_id: atomic::AtomicI64::new(0),
-        }
+    pub fn new(c: redis::Client) -> Self {
+        MemoryStore { redis: c }
     }
 }
 
 impl Store for MemoryStore {
     fn generate_server_id(&self) -> Result<i64, io::Error> {
-        Ok(self.server_id.fetch_add(1, atomic::Ordering::SeqCst))
+        let conn = self.redis.get_connection();
+        return if conn.is_err() {
+            Err(io::Error::new(
+                io::ErrorKind::Other,
+                "redis connection error",
+            ))
+        } else {
+            let mut conn = conn.unwrap();
+            let result = redis::cmd("incr").arg("server_id").query::<i64>(&mut conn);
+            if result.is_err() {
+                Err(io::Error::new(io::ErrorKind::Other, "redis incr error"))
+            } else {
+                Ok(result.unwrap())
+            }
+        };
     }
-    fn register_server(&self, s :Server) -> Result<(), io::Error> {
+    fn register_server(&self, s: Server) -> Result<(), io::Error> {
         let _ = s;
         Ok(())
     }
@@ -42,7 +52,6 @@ impl Store for MemoryStore {
     }
     fn register_room(&self, r: Room) -> Result<(), io::Error> {
         let _ = r;
-
         Ok(())
     }
     fn unregister_room(&self, r: Room) -> Result<(), io::Error> {
@@ -51,17 +60,24 @@ impl Store for MemoryStore {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_generate_server_id() {
-        let m = MemoryStore::new();
-        let id = m.generate_server_id().unwrap();
-        assert_eq!(id, 0);
+        let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+        let mut con = client.get_connection().unwrap();
+
+        redis::cmd("del")
+            .arg("server_id")
+            .query::<i64>(&mut con)
+            .unwrap();
+
+        let m = MemoryStore::new(client);
         let id = m.generate_server_id().unwrap();
         assert_eq!(id, 1);
+        let id = m.generate_server_id().unwrap();
+        assert_eq!(id, 2);
     }
 }
